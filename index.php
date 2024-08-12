8b504e91b88037350537cfd90d0cc1db64ee24c9
XboXChristian <Chrtobias4780@gmail.com>
2024-08-12 16:00:11 +0200

    Frontpage text start

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?php include "includes/header.php" ?>
 
-<h1>hej med dig</h1>
+<div class="container-fluid bg-front ">
+    <div class="row ">
+
+        <div class="top-30 top position-absolute text-center">
+                <h6 class="text-white">Velkommen til Nordic Webworks</h6>
+                <h1 class="text-white fw-bold">Har din virksomhed brug for en løsning?</h1>
+
+            <div class="mt-4">
+                <div class="me-3 btn-f d-inline-flex justify-content-center align-items-center">
+                    <p class=" text-white text-center m-0">Kontakt os</p></div>
+
+                <div class=" btn-f d-inline-flex justify-content-center align-items-center">
+                    <p class=" text-white text-center m-0">Vores Arbejde</p></div>
+            </div>
 
 
+        </div>
+
+
+
+    </div>
+</div>
 
 
 <?php include "includes/footer.php" ?>
