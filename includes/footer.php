724f534b77594494df195d405df37c2be7cda2a3
Arnsdorf <38246817+Arnsdorf@users.noreply.github.com>
2024-08-13 13:22:48 +0200

    Navbar 1.4

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,22 @@
+<script>
+    let lastScrollTop = 0;
+    const navbar = document.querySelector('.navbar');
 
+    window.addEventListener('scroll', function() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop && scrollTop > 100) {
+            // Scroller ned, skjul navbar
+            navbar.classList.add('hidden');
+        } else {
+            // Scroller op, vis navbar
+            navbar.classList.remove('hidden');
+        }
+
+        lastScrollTop = scrollTop;
+    });
+
+</script>
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php include "includes/header.php" ?>
 
-<div class="container-fluid bg-front ">
-    <div class="row ">
-
-        <div class="top-30 position-absolute text-center ">
-                <h6 class="text-white font-front-s d-md-none">━━  Velkommen til Nordic Webworks ━━━━━</h6>
-                <h1 class="text-white fw-bold d-md-none font-type">Har din virksomhed brug for en løsning?</h1>
+<div class="container-fluid bg-front">
+    <div class="row">
+        <div class="top-30 position-absolute text-center">
+            <h6 class="text-white font-front-s d-md-none">━━  Velkommen til Nordic Webworks ━━━━━</h6>
+            <h1 class="text-white fw-bold d-md-none font-type">Har din virksomhed brug for en løsning?</h1>
 
             <div class="mt-4 d-md-none">
                 <div class="me-3 btn-f d-inline-flex justify-content-center align-items-center text-center m-0 drip">Kontakt os</div>
@@ -13,7 +12,7 @@
             </div>
             <!-------Tablet and pc size-------------------------->
             <div class="ms-13">
-                <div class="w-43 text-md-start ">
+                <div class="w-43 text-md-start">
                     <h5 class="text-white d-none d-md-block d-flex">━━  Velkommen til Nordic Webworks ━━━━━</h5>
                     <h1 class="text-white fw-bold d-none d-md-block front-font-b d-flex">Har din virksomhed brug for en løsning?</h1>
                 </div>
@@ -23,15 +22,60 @@
                     <div class="btn-f d-inline-flex justify-content-center align-items-center text-center m-0">Cases</div>
                 </div>
             </div>
-
         </div>
-
-
-
-
-
     </div>
 </div>
 
+<!-- Dummy Content Section 1 -->
+<div class="container my-5">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Om os</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque imperdiet pulvinar dui, ac vulputate nulla posuere eu. Integer at libero sit amet sapien tempus auctor. Curabitur vel libero quis turpis egestas pharetra.</p>
+        </div>
+        <div class="col-md-6">
+            <img src="https://via.placeholder.com/600x400" class="img-fluid" alt="Placeholder Image">
+        </div>
+    </div>
+</div>
+
+<!-- Dummy Content Section 2 -->
+<div class="container my-5 bg-light p-4">
+    <div class="row">
+        <div class="col-md-6">
+            <img src="https://via.placeholder.com/600x400" class="img-fluid" alt="Placeholder Image">
+        </div>
+        <div class="col-md-6">
+            <h2>Vores Services</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod, libero vel dapibus luctus, orci nisl tincidunt magna, eget tincidunt erat justo eget lacus. Aenean ac lectus a felis egestas dapibus.</p>
+        </div>
+    </div>
+</div>
+
+<!-- Dummy Content Section 3 -->
+<div class="container my-5">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Mød Holdet</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget eros felis. Vivamus tristique, purus eu volutpat vehicula, justo leo condimentum est, non condimentum nisi velit id nunc. Sed tincidunt mauris sit amet odio bibendum aliquet.</p>
+        </div>
+        <div class="col-md-6">
+            <img src="https://via.placeholder.com/600x400" class="img-fluid" alt="Placeholder Image">
+        </div>
+    </div>
+</div>
+
+<!-- Dummy Content Section 4 -->
+<div class="container my-5 bg-light p-4">
+    <div class="row">
+        <div class="col-md-6">
+            <img src="https://via.placeholder.com/600x400" class="img-fluid" alt="Placeholder Image">
+        </div>
+        <div class="col-md-6">
+            <h2>Kontakt</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque commodo, dui nec fermentum viverra, velit lacus varius purus, nec volutpat purus lacus a justo. Nunc sit amet lacus eu justo vulputate feugiat.</p>
+        </div>
+    </div>
+</div>
 
 <?php include "includes/footer.php" ?>
