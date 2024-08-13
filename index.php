0f62c1e2a5d4631ec596224b3bdf37d1fcf9087d
XboXChristian <Chrtobias4780@gmail.com>
2024-08-13 13:53:27 +0200

    Working Point

--- index.php
+++ index.php
@@ -20,66 +20,21 @@
                 <h1 class="text-white d-none d-md-block front-font-b d-flex font-type">Har din virksomhed brug for en løsning?</h1>
             </div>
 
-            <div class="mt-4 text-md-start ms-md-8 d-none d-md-block">
-                <div class="me-3 btn-f d-inline-flex justify-content-center align-items-center text-center m-0 drip">
-                    <span>Kontakt os</span>
+                <div class="mt-4 text-md-start ms-md-8 d-none d-md-block">
+                    <div class="me-3 btn-f d-inline-flex justify-content-center align-items-center text-center m-0 drip">
+                        <span>Kontakt os</span>
+                    </div>
+                    <div class="btn-f d-inline-flex justify-content-center align-items-center text-center m-0">
+                        <span>Cases</span></div>
                 </div>
-                <div class="btn-f d-inline-flex justify-content-center align-items-center text-center m-0">
-                    <span>Cases</span></div>
             </div>
-        </div>
-    </div>
-</div>
 
-<!-- Dummy Content Section 1 --->
-<div class="container my-5">
-    <div class="row">
-        <div class="col-md-6">
-            <h2>Om os</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque imperdiet pulvinar dui, ac vulputate nulla posuere eu. Integer at libero sit amet sapien tempus auctor. Curabitur vel libero quis turpis egestas pharetra.</p>
         </div>
-        <div class="col-md-6">
-            <img src="https://via.placeholder.com/600x400" class="img-fluid" alt="Placeholder Image">
-        </div>
-    </div>
-</div>
 
-<!-- Dummy Content Section 2 -->
-<div class="container my-5 bg-light p-4">
-    <div class="row">
-        <div class="col-md-6">
-            <img src="https://via.placeholder.com/600x400" class="img-fluid" alt="Placeholder Image">
-        </div>
-        <div class="col-md-6">
-            <h2>Vores Services</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod, libero vel dapibus luctus, orci nisl tincidunt magna, eget tincidunt erat justo eget lacus. Aenean ac lectus a felis egestas dapibus.</p>
-        </div>
-    </div>
-</div>
 
-<!-- Dummy Content Section 3 -->
-<div class="container my-5">
-    <div class="row">
-        <div class="col-md-6">
-            <h2>Mød Holdet</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget eros felis. Vivamus tristique, purus eu volutpat vehicula, justo leo condimentum est, non condimentum nisi velit id nunc. Sed tincidunt mauris sit amet odio bibendum aliquet.</p>
-        </div>
-        <div class="col-md-6">
-            <img src="https://via.placeholder.com/600x400" class="img-fluid" alt="Placeholder Image">
-        </div>
-    </div>
-</div>
 
-<!-- Dummy Content Section 4 -->
-<div class="container my-5 bg-light p-4">
-    <div class="row">
-        <div class="col-md-6">
-            <img src="https://via.placeholder.com/600x400" class="img-fluid" alt="Placeholder Image">
-        </div>
-        <div class="col-md-6">
-            <h2>Kontakt</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque commodo, dui nec fermentum viverra, velit lacus varius purus, nec volutpat purus lacus a justo. Nunc sit amet lacus eu justo vulputate feugiat.</p>
-        </div>
+
+
     </div>
 </div>
 
