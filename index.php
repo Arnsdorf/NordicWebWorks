12466e896177aea5c6811aad60c950be535365f6
XboXChristian <Chrtobias4780@gmail.com>
2024-08-13 10:19:14 +0200

    Frontpage skalering done. basic.

--- index.php
+++ index.php
@@ -3,20 +3,36 @@
 <div class="container-fluid bg-front ">
     <div class="row ">
 
-        <div class="top-30 top position-absolute text-center">
-                <h6 class="text-white">Velkommen til Nordic Webworks</h6>
-                <h1 class="text-white fw-bold">Har din virksomhed brug for en løsning?</h1>
+        <div class="top-30 position-absolute text-center">
+                <h6 class="text-white font-front-s d-md-none">━━  Velkommen til Nordic Webworks ━━━━━</h6>
+                <h1 class="text-white fw-bold d-md-none">Har din virksomhed brug for en løsning?</h1>
 
-            <div class="mt-4">
+            <div class="mt-4 d-md-none">
                 <div class="me-3 btn-f d-inline-flex justify-content-center align-items-center">
                     <p class=" text-white text-center m-0">Kontakt os</p></div>
 
                 <div class=" btn-f d-inline-flex justify-content-center align-items-center">
-                    <p class=" text-white text-center m-0">Vores Arbejde</p></div>
+                    <p class=" text-white text-center m-0">Cases</p></div>
             </div>
 
+            <div class="ms-13">
+                <div class="w-50 text-md-start ">
+                    <h5 class="text-white d-none d-md-block d-flex">━━  Velkommen til Nordic Webworks ━━━━━</h5>
+                    <h1 class="text-white fw-bold d-none d-md-block front-font-b d-flex">Har din virksomhed brug for en løsning?</h1>
+                </div>
+
+                <div class="mt-4 text-md-start ms-md-8 d-none d-md-block">
+                    <div class="me-3 btn-f d-inline-flex justify-content-center align-items-center">
+                        <p class=" text-white text-center m-0">Kontakt os</p></div>
+
+                    <div class=" btn-f d-inline-flex justify-content-center align-items-center ">
+                        <p class=" text-white text-center m-0">Cases</p></div>
+                </div>
+            </div>
 
         </div>
+
+
 
 
 
@@ -25,3 +41,4 @@
 
 
 <?php include "includes/footer.php" ?>
+<h1 class="text-white fw-bold front-font-b d-none d-md-block">Har din virksomhed brug for en løsning?</h1>
