2293e0b9d27c08ca7603c96d71b88d63afe7aad2
Arnsdorf <38246817+Arnsdorf@users.noreply.github.com>
2024-08-19 14:01:54 +0200

    Mød Holdet 1.3

---
+++ meettheteam.php
@@ -0,0 +1,47 @@
+<?php include "includes/header.php" ?>
+<div class="container-fluid h-100">
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3 p-0">
+            <div class="box position-relative m-0">
+                <img src="uploads/CrewGoodFellas.jpg" alt="Billede 1" class="img-fluid shadow">
+                <div class="overlay overlay-1 d-flex flex-column justify-content-end text-white p-4">
+                    <h2 class="display-7">Loay Brooklyn Haijo</h2>
+                    <h5 class="text-blue mb-3 fw-bold">Fullstack Dev</h5>
+                    <p class="small mb-0">Loay er en erfaren Fullstack udvikler med ekspertise i både FrontEnd og BackEnd. Han har en særlig styrke i PHP og databaser som MySQL, samt solid erfaring med Wordpress og React.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3 p-0">
+            <div class="box position-relative m-0">
+                <img src="uploads/CrewGoodFellas.jpg" alt="Billede 2" class="img-fluid shadow">
+                <div class="overlay overlay-2 d-flex flex-column justify-content-end text-white p-4">
+                    <h2 class="display-7">Loay Brooklyn Haijo</h2>
+                    <h5 class="text-blue mb-3 fw-bold">Fullstack Dev</h5>
+                    <p class="small mb-0">Loay er en erfaren Fullstack udvikler med ekspertise i både FrontEnd og BackEnd. Han har en særlig styrke i PHP og databaser som MySQL, samt solid erfaring med Wordpress og React.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3 p-0">
+            <div class="box position-relative m-0">
+                <img src="uploads/CrewGoodFellas.jpg" alt="Billede 3" class="img-fluid shadow">
+                <div class="overlay overlay-3 d-flex flex-column justify-content-end text-white p-4">
+                    <h2 class="display-7">Loay Brooklyn Haijo</h2>
+                    <h5 class="text-blue mb-3 fw-bold">Fullstack Dev</h5>
+                    <p class="small mb-0">Loay er en erfaren Fullstack udvikler med ekspertise i både FrontEnd og BackEnd. Han har en særlig styrke i PHP og databaser som MySQL, samt solid erfaring med Wordpress og React.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3 p-0">
+            <div class="box position-relative m-0">
+                <img src="uploads/CrewGoodFellas.jpg" alt="Billede 4" class="img-fluid shadow">
+                <div class="overlay overlay-4 d-flex flex-column justify-content-end text-white p-4">
+                    <h2 class="display-7">Loay Brooklyn Haijo</h2>
+                    <h5 class="text-blue mb-3 fw-bold">Fullstack Dev</h5>
+                    <p class="small mb-0">Loay er en erfaren Fullstack udvikler med ekspertise i både FrontEnd og BackEnd. Han har en særlig styrke i PHP og databaser som MySQL, samt solid erfaring med Wordpress og React.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include "includes/footer.php" ?>
