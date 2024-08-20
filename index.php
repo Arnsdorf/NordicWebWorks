da6ea93284e87f9aa3d6b71dd713b075e385839e
Arnsdorf <38246817+Arnsdorf@users.noreply.github.com>
2024-08-20 14:51:06 +0200

    services 1.0

--- index.php
+++ index.php
@@ -1,118 +1,123 @@
-<?php include "includes/header.php" ?>
+    <?php include "includes/header.php" ?>
 
 
-<div class="container-fluid bg-front net-grid">
-    <div class="row">
+    <div class="container-fluid bg-front net-grid">
+        <div class="row">
 
-        <!-------Boxes-and-socials--------------->
-        <div class="position-absolute bottom-0 ps-4 d-md-none">
-            <div class=""><i class="bi bi-instagram text-white social" style="font-size: 1.2rem"></i></div>
-            <div class="mt-2"><i class="bi bi-linkedin text-white social" style="font-size: 1.2rem"></i></div>
-            <div class="mt-2"><i class="bi bi-twitter text-white social" style="font-size: 1.2rem"></i></div>
-            <div class="mt-2 mb-2"><i class="bi bi-facebook text-white social" style="font-size: 1.2rem"></i></div>
-        </div>
-
-
-        <div class="position-absolute bottom-0 ps-4 d-none d-md-block">
-                <div class=""><i class="bi bi-instagram social" style="font-size: 1.4rem"></i></div>
-                <div class="mt-3"><i class="bi bi-linkedin social" style="font-size: 1.4rem"></i></div>
-                <div class="mt-3"><i class="bi bi-twitter social" style="font-size: 1.4rem"></i></div>
-                <div class="mt-3 mb-3"><i class="bi bi-facebook social" style="font-size: 1.4rem"></i></div>
-        </div>z
-
-        <div class="position-absolute bottom-0 pb-3 ps-3">
-                <div class="box-s position-absolute bottom-60 ms-5"></div>
-                <div class="box-b"></div>
-        </div>
-        <!-------Tlf-frontpage----------->
-        <div class="text-center position-absolute top-25">
-                <h6 class="font-front-s d-md-none">━━     Velkommen til Nordic Webworks ━━━━━</h6>
-                <h1 class="text-white fw-bold d-md-none font-type">Har <span class="font-drip">din
-                    </span> virksomhed brug for en løsning<span class="font-drip">?</span></h1>
-
-        <div class="mt-4 d-md-none">
-            <div class="me-3 btn-f d-inline-flex justify-content-center align-items-center text-center m-0 drip">
-                <span>Kontakt os</div>
-            <div class="btn-f d-inline-flex justify-content-center align-items-center text-center m-0">
-                <span>Cases</span></div>
-        </div>
-
-
-        <!-------Tablet and pc size----------------------------------->
-        <div class="ms-13">
-            <!-----TABLET------>
-            <div class="w-51 text-md-start d-none d-md-block d-xl-none">
-                <h6 class="d-none d-md-block d-flex" style="color: #636363;">━━  Velkommen til Nordic Webworks ━━━━━</h6>
-                <h1 class="text-white d-none d-md-block front-font-b d-flex font-type">Har <span class="font-drip">din
-                    </span> virksomhed brug for en løsning<span class="font-drip">?</span></h1>
-            </div>
-            <!-----PC--------->
-            <div class="w-43 text-md-start d-md-none d-xl-block">
-                <h5 class="d-none d-md-block d-flex" style="color: #636363;">━━  Velkommen til Nordic Webworks ━━━━━</h5>
-                <h1 class="text-white d-none d-md-block front-font-b d-flex font-type">Har <span class="font-drip">din</span>
-                    virksomhed brug for en løsning<span class="font-drip">?</span></h1>
+            <!-------Boxes-and-socials--------------->
+            <div class="position-absolute bottom-0 ps-4 d-md-none">
+                <div class=""><i class="bi bi-instagram text-white social" style="font-size: 1.2rem"></i></div>
+                <div class="mt-2"><i class="bi bi-linkedin text-white social" style="font-size: 1.2rem"></i></div>
+                <div class="mt-2"><i class="bi bi-twitter text-white social" style="font-size: 1.2rem"></i></div>
+                <div class="mt-2 mb-2"><i class="bi bi-facebook text-white social" style="font-size: 1.2rem"></i></div>
             </div>
 
-            <div class="mt-4 text-md-start ms-md-8 d-none d-md-block">
-                <div class="me-3 btn-f d-inline-flex justify-content-center align-items-center text-center m-0 drip">
-                    <span>Kontakt os</span>
+
+            <div class="position-absolute bottom-0 ps-4 d-none d-md-block">
+                    <div class=""><i class="bi bi-instagram social" style="font-size: 1.4rem"></i></div>
+                    <div class="mt-3"><i class="bi bi-linkedin social" style="font-size: 1.4rem"></i></div>
+                    <div class="mt-3"><i class="bi bi-twitter social" style="font-size: 1.4rem"></i></div>
+                    <div class="mt-3 mb-3"><i class="bi bi-facebook social" style="font-size: 1.4rem"></i></div>
+            </div>
+            <!-------Boxes--------------->
+            <div class="position-absolute bottom-0 pb-3 ps-3">
+                <div class="box-s position-absolute bottom-60 ms-5" style="pointer-events: none;"></div>
+                <div class="box-b" style="pointer-events: none;"></div>
+            </div>
+
+            <!-------Tlf-frontpage----------->
+            <div class="text-center position-absolute top-25">
+                    <h6 class="font-front-s d-md-none">━━     Velkommen til Nordic Webworks ━━━━━</h6>
+                    <h1 class="text-white fw-bold d-md-none font-type">Har <span class="font-drip">din
+                        </span> virksomhed brug for en løsning<span class="font-drip">?</span></h1>
+
+            <div class="mt-4 d-md-none">
+                <div class="me-3 btn-f fw-bold small d-inline-flex justify-content-center align-items-center text-center m-0 drip">
+                    <span>KONTAKT</div>
+                <div class="btn-f fw-bold small d-inline-flex justify-content-center align-items-center text-center m-0">
+                    <span>CASES</span></div>
+            </div>
+
+
+            <!-------Tablet and pc size----------------------------------->
+            <div class="ms-13">
+                <!-----TABLET------>
+                <div class="w-51 text-md-start d-none d-md-block d-xl-none">
+                    <h6 class="d-none d-md-block d-flex" style="color: #636363;">━━  Velkommen til Nordic Webworks ━━━━━</h6>
+                    <h1 class="text-white d-none d-md-block front-font-b d-flex font-type">Har <span class="font-drip">din
+                        </span> virksomhed brug for en løsning<span class="font-drip">?</span></h1>
                 </div>
-                <div class="btn-f d-inline-flex justify-content-center align-items-center text-center m-0">
-                    <span>Cases</span></div>
+                <!-----PC--------->
+                <div class="w-43 text-md-start d-md-none d-xl-block">
+                    <h5 class="d-none d-md-block d-flex" style="color: #636363;">━━  Velkommen til Nordic Webworks ━━━━━</h5>
+                    <h1 class="text-white d-none d-md-block front-font-b d-flex font-type">Har <span class="font-drip">din</span>
+                        virksomhed brug for en løsning<span class="font-drip">?</span></h1>
+                </div>
+
+                <div class="mt-4 text-md-start ms-md-8 d-none d-md-block">
+                    <div class="me-3 fw-bold small btn-f d-inline-flex justify-content-center align-items-center text-center m-0 drip">
+                        <span>KONTAKT</span>
+                    </div>
+                    <div class="btn-f fw-bold small d-inline-flex justify-content-center align-items-center text-center m-0">
+                        <span>CASES</span></div>
+                </div>
             </div>
+
         </div>
 
+
+
+
+
+        </div>
+        </div>
+
+        <div class="container-fluid bg-cases">
+            <div class="row p-0 m-0">
+
+            <div class="col-12 d-flex case-bg align-items-center p-0">
+
+                <div class="box position-relative p-0 col-2">
+                    <img class="img-box position-absolute" src="uploads/1396x2500-px-alleyway-artwork-city-cityscape-digital-art-1346043-wallhere.com.jpg" alt="a balloon">
+                    <div class="hover-box position-absolute">
+                        <h2 class="font-custom font-box pt-3 ps-2 case-font">BuildMate</h2>
+                    </div>
+                </div>
+
+                <div class="box position-relative p-0 ms-1">
+                    <img class="img-box position-absolute" src="uploads/ff8201d6aea4092207b503cf9cbc0df0.jpg" alt="a balloon">
+                    <div class="hover-box position-absolute">
+                        <h2 class="font-custom font-box pt-3 ps-2 case-font">BuildMate</h2>
+                    </div>
+                </div>
+
+                <div class="box position-relative p-0 ms-1">
+                    <img class="img-box position-absolute" src="uploads/il_570xN.5283973056_77vu.webp" alt="a balloon">
+                    <div class="hover-box position-absolute">
+                        <h2 class="font-custom font-box pt-3 ps-2 case-font">BuildMate</h2>
+                    </div>
+                </div>
+
+                <div class="box position-relative p-0 ms-1">
+                    <img class="img-box position-absolute" src="uploads/cyberpunk-market-set-in-ancient-ruins-4k-hc-1125x2436.jpg" alt="a balloon">
+                    <div class="hover-box position-absolute">
+                        <h2 class="font-custom font-box pt-3 ps-2 case-font">BuildMate</h2>
+                    </div>
+                </div>
+
+                <div class="box position-relative p-0 ms-1" style="width: 15vw;">
+                    <img class="img-box position-absolute" src="uploads/1396x2500-px-alleyway-artwork-city-cityscape-digital-art-1346043-wallhere.com.jpg" alt="a balloon">
+                    <div class="hover-box position-absolute">
+                        <h2 class="text-black font-box pt-3 ps-2 case-font">BuildMate</h2>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
     </div>
 
-</div>
-    </div>
 
-    <div class="container-fluid bg-cases">
-        <div class="row p-0 m-0">
-
-        <div class="col-12 d-flex case-bg align-items-center p-0">
-
-            <div class="box position-relative p-0 col-2">
-                <img class="img-box position-absolute" src="uploads/1396x2500-px-alleyway-artwork-city-cityscape-digital-art-1346043-wallhere.com.jpg" alt="a balloon">
-                <div class="hover-box position-absolute">
-                    <h2 class="font-custom font-box pt-3 ps-2 case-font">BuildMate</h2>
-                </div>
-            </div>
-
-            <div class="box position-relative p-0 ms-1">
-                <img class="img-box position-absolute" src="uploads/ff8201d6aea4092207b503cf9cbc0df0.jpg" alt="a balloon">
-                <div class="hover-box position-absolute">
-                    <h2 class="font-custom font-box pt-3 ps-2 case-font">BuildMate</h2>
-                </div>
-            </div>
-
-            <div class="box position-relative p-0 ms-1">
-                <img class="img-box position-absolute" src="uploads/il_570xN.5283973056_77vu.webp" alt="a balloon">
-                <div class="hover-box position-absolute">
-                    <h2 class="font-custom font-box pt-3 ps-2 case-font">BuildMate</h2>
-                </div>
-            </div>
-
-            <div class="box position-relative p-0 ms-1">
-                <img class="img-box position-absolute" src="uploads/cyberpunk-market-set-in-ancient-ruins-4k-hc-1125x2436.jpg" alt="a balloon">
-                <div class="hover-box position-absolute">
-                    <h2 class="font-custom font-box pt-3 ps-2 case-font">BuildMate</h2>
-                </div>
-            </div>
-
-            <div class="box position-relative p-0 ms-1" style="width: 15vw;">
-                <img class="img-box position-absolute" src="uploads/1396x2500-px-alleyway-artwork-city-cityscape-digital-art-1346043-wallhere.com.jpg" alt="a balloon">
-                <div class="hover-box position-absolute">
-                    <h2 class="text-black font-box pt-3 ps-2 case-font">BuildMate</h2>
-                </div>
-            </div>
-
-        </div>
-
-
-    </div>
-</div>
-
-
-    <script src="js/show.js"></script>
-<?php include "includes/footer.php" ?>
+        <script src="js/show.js"></script>
+    <?php include "includes/footer.php" ?>
