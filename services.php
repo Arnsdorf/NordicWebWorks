bdf307d6a0b526d2f94a77739c00b4afe3f9083e
Arnsdorf <38246817+Arnsdorf@users.noreply.github.com>
2024-08-22 12:39:59 +0200

    services 1.5

--- services.php
+++ services.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="row d-flex justify-content-center">
                 <div class="col-md-10 col-lg-6 text-center">
-                    <h1 class="text-white display-6 fw-bold">Services & Ydelser</h1>
+                    <h1 class="text-white display-6 fw-bold"><span style="color: #683690">Services</span> & Ydelser</h1>
                     <p class="text-white-50 small mt-3">
                         Vi tilbyder en bred vifte af ydelser, der er skræddersyet
                         til at imødekomme dine behov. Fra webudvikling til digital strategi,
@@ -19,18 +19,18 @@
         <div class="container">
             <div class="row">
                 <!-- Card 1 -->
-                <div class="col-md-6 col-lg-3 mb-4 d-flex justify-content-center">
-                    <div class="card card-hover bg-cases h-100">
+                <div class="col-md-6 col-lg-3 mb-4">
+                    <div class="card border-0 card-hover bg-cases h-100">
                         <div class="card-body text-white d-flex flex-column">
                             <div>
-                                <img style="width: 40px" class="shadow mb-3" src="uploads/icons/coding.png" alt="coding">
-                                <h5 class="card-title fw-bold">Fremstilling af website</h5>
+                                <img style="width: 40px" class="shadow pt-3 mb-3" src="uploads/icons/coding.png" alt="coding">
+                                <h5 class="card-title fw-bold">Frontend Løsning</h5>
                                 <p class="card-text fw-bold small text-white-50">
                                     React.js • Angular • Vue.js
                                 </p>
                             </div>
                             <div class="mt-auto">
-                                <p class="card-text text-white-50 small">
+                                <p class="card-text text-white-50 mt-2 small">
                                     Vi leverer responsiv og dynamisk frontend-udvikling, der sikrer en problemfri oplevelse på tværs af alle enheder.
                                 </p>
                                 <div class="btn-f bg-transparent d-inline-flex border-white fw-bold justify-content-center align-items-center text-center">
@@ -43,17 +43,17 @@
 
                 <!-- Card 2 -->
                 <div class="col-md-6 col-lg-3 mb-4 d-flex justify-content-center">
-                    <div class="card card-hover bg-cases h-100">
+                    <div class="card border-0 card-hover bg-cases h-100">
                         <div class="card-body text-white d-flex flex-column">
                             <div>
-                                <img style="width: 40px" class="shadow mb-3" src="uploads/icons/database.png" alt="coding">
+                                <img style="width: 40px" class="shadow mb-3 pt-3" src="uploads/icons/database.png" alt="coding">
                                 <h5 class="card-title fw-bold">Backend Udvikling</h5>
                                 <p class="card-text fw-bold small text-white-50">
                                     PHP • Databaser • API'er
                                 </p>
                             </div>
                             <div class="mt-auto">
-                                <p class="card-text small text-white-50">
+                                <p class="card-text small mt-2 text-white-50">
                                     Vores backend-løsninger sikrer, at din hjemmeside eller applikation fungerer effektivt og sikkert i baggrunden.
                                 </p>
                                 <div class="btn-f bg-transparent d-inline-flex border-white fw-bold justify-content-center align-items-center text-center">
@@ -65,18 +65,18 @@
                 </div>
 
                 <!-- Card 3 -->
-                <div class="col-md-6 col-lg-3 mb-4 d-flex justify-content-center">
-                    <div class="card card-hover bg-cases h-100">
+                <div class="col-md-6 col-lg-3 mb-4">
+                    <div class="card border-0 card-hover bg-cases h-100">
                         <div class="card-body text-white d-flex flex-column">
                             <div>
-                                <img style="width: 40px" class="shadow mb-3" src="uploads/icons/interface.png" alt="coding">
-                                <h5 class="card-title fw-bold">Webdesign & UX/UI Design</h5>
+                                <img style="width: 40px" class="shadow mb-3 pt-3" src="uploads/icons/interface.png" alt="coding">
+                                <h5 class="card-title fw-bold">Webdesign & UX/UI</h5>
                                 <p class="card-text fw-bold small text-white-50">
                                     Identitet • Prototyper • Testing
                                 </p>
                             </div>
                             <div class="mt-auto">
-                                <p class="card-text text-white-50 small">
+                                <p class="card-text mt-2 text-white-50 small">
                                     Vi skaber moderne og intuitive webdesigns, der engagerer brugere og konverterer besøgende til kunder.
                                 </p>
                                 <div class="btn-f bg-transparent d-inline-flex border-white fw-bold justify-content-center align-items-center text-center">
@@ -88,18 +88,18 @@
                 </div>
 
                 <!-- Card 4 -->
-                <div class="col-md-6 col-lg-3 mb-4 d-flex justify-content-center">
-                    <div class="card card-hover bg-cases h-100">
+                <div class="col-md-6 col-lg-3 mb-4">
+                    <div class="card border-0 card-hover bg-cases h-100">
                         <div class="card-body text-white d-flex flex-column">
                             <div>
-                                <img style="width: 40px" class="shadow mb-3" src="uploads/icons/api.png" alt="coding">
+                                <img style="width: 40px" class="shadow mb-3 pt-3" src="uploads/icons/api.png" alt="coding">
                                 <h5 class="card-title fw-bold">API Integration</h5>
                                 <p class="card-text fw-bold small text-white-50">
                                     API Endpoint • RESTful • API Nøgle
                                 </p>
                             </div>
                             <div class="mt-auto">
-                                <p class="card-text text-white-50 small">
+                                <p class="card-text mt-2 text-white-50 small">
                                     Vi integrerer tredjeparts API'er for at forbedre funktionaliteten og effektiviteten af din applikation.
                                 </p>
                                 <div class="btn-f bg-transparent d-inline-flex border-white fw-bold justify-content-center align-items-center text-center">
