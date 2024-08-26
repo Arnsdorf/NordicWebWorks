2fa1ef097f47c1ff47c7448863fe160921560b72
XboXChristian <Chrtobias4780@gmail.com>
2024-08-26 13:43:12 +0200

    cases rework plus scaling

--- caseinfo.php
+++ caseinfo.php
@@ -29,25 +29,66 @@ require "settings/init.php";
     <div class="row p-0 m-0">
 
 
+        <div class="col-12 m-0 p-0 position-relative d-md-none" style="height: 25vh;">
+            <div class="impro-nav col-12" style="height: 4vh; position: absolute; z-index: 2;"> </div>
+            <img class="case-info-img col-12" src="uploads/chr/luca-bravo-9l_326FISzk-unsplash.jpg" alt="#" style="object-fit: cover; position: absolute; top: 0; z-index: 1;">
+        </div>
 
-        <div class="col-4 m-0 p-0 position-relative" style="height: 100vh;">
-            <div class="bg-danger" style="height: 100vh; width: 30%; position: absolute; top: 0; left: 0; z-index: 2;"> </div>
-            <img class="case-info-img w-100 h-100" src="uploads/nature-vertical-yefjvpp5yr2mwqjq.jpg" alt="#" style="object-fit: cover; position: absolute; top: 0; left: 30%; z-index: 1;">
+        <div class="col-12 bg-custom-dark m-0 p-0 d-md-none">
+                <div class="text-white d-flex justify-content-center mt-3 mb-3">
+                    <div class="row" style=";color: #7c7c7c">
+                        <h6 class="fw-light col" style="letter-spacing: 0.1rem;line-height: 1.5rem">.Php</h6>
+                        <h6 class="fw-light ms-4 me-4 col" style="letter-spacing: 0.1rem;line-height: 1.5rem">.SQL</h6>
+                        <h6 class="fw-light col" style="letter-spacing: 0.1rem;line-height: 1.5rem">.JavaScript</h6>
+                    </div>
+            </div>
+
+            <div class="ms-4 me-4 text-white mt-4">
+                <h1 class="fw-bold">BuildMate</h1>
+                <h6 class="fw-light pt-1 " style="letter-spacing: 0.1rem;line-height: 1.5rem;color: #7c7c7c">Computer bygnings projekt.</h6>
+                <h6 class="fw-light pt-3 " style="letter-spacing: 0.1rem;line-height: 1.5rem">BuildMate opstod da en flok mennesker beslutter sig for computerbygning var for kompliceret.
+                    En digital løsning blev kreaeret af denne folk mennesker, i håber om at alle en dag skulle kunne nyde den digitale verden
+                    uden bekymring for om deres udstyr holder dem tilbage.</h6>
+            </div>
+        </div>
+
+        <div class="col bg-custom-dark m-0 p-0 d-flex ms-3 mt-4 d-md-none">
+            <h1 class="fw-bold justify-content-center"
+                style="font-size: 2.2rem;  letter-spacing: 0.4rem; color: #343434">Case-Information</h1>
         </div>
 
 
 
-        <div class="col-6 bg-custom-dark" style="height: 100vh; z-index: 2">
-            <div class="top-35 position-absolute ms-5 text-white mt-1">
+        <!---------pc-lg-and-md------------------------------->
+        <div class="col-4 m-0 p-0 position-relative d-none d-md-block" style="height: 100vh;">
+            <div class="impro-nav col-2" style="height: 100vh; position: absolute; z-index: 2;"> </div>
+            <img class="case-info-img col-12" src="uploads/chr/luca-bravo-9l_326FISzk-unsplash.jpg" alt="#" style="object-fit: cover; position: absolute; top: 0; z-index: 1;">
+        </div>
+
+        <div class="col-6 bg-custom-dark m-0 p-0 d-none d-md-block">
+
+            <div class="col bg-custom-dark p-0 m-0 d-flex mt-3">
+                <div class="ms-5 text-white d-flex align-items-end">
+                    <div class="row justify-content-between" style=";color: #7c7c7c">
+                        <h6 class="fw-light col-2" style="letter-spacing: 0.1rem;line-height: 1.5rem">.Php</h6>
+                        <h6 class="fw-light ms-5 me-5 col-2" style="letter-spacing: 0.1rem;line-height: 1.5rem">.SQL</h6>
+                        <h6 class="fw-light col-2" style="letter-spacing: 0.1rem;line-height: 1.5rem">.JavaScript</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="pt-17 ms-5 text-white mt-1">
                 <h1 class="fw-bold" style="font-size: 4rem">BuildMate</h1>
+                <h6 class="fw-light pt-3 col-8" style="letter-spacing: 0.1rem;line-height: 1.5rem;color: #7c7c7c">Computer bygnings projekt.</h6>
                 <h6 class="fw-light pt-3 col-8" style="letter-spacing: 0.1rem;line-height: 1.5rem">BuildMate opstod da en flok mennesker beslutter sig for computerbygning var for kompliceret.
                 En digital løsning blev kreaeret af denne folk mennesker, i håber om at alle en dag skulle kunne nyde den digitale verden
                 uden bekymring for om deres udstyr holder dem tilbage.</h6>
             </div>
         </div>
 
-        <div class="col-2 bg-custom-dark d-flex justify-content-center m-0 p-0 align-items-center" style="height: 100vh">
-            <h1 class="fw-bold align-items-center vertical-text" style="writing-mode: vertical-rl; font-size: 3.3rem;  letter-spacing: 0.4rem; color: #343434">Case-Information</h1>
+        <div class="col bg-custom-dark m-0 p-0 d-none d-md-block">
+            <h1 class="fw-bold"
+                style="writing-mode: vertical-rl; font-size: 3.3rem;  letter-spacing: 0.4rem; color: #343434">Case-Information</h1>
         </div>
 
     </div>
