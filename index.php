2aad75d817d778d10db52955241c50991046769f
XboXChristian <Chrtobias4780@gmail.com>
2024-08-29 13:15:50 +0200

    Tweeking on frontpage, cases, fonts and animations.

--- index.php
+++ index.php
@@ -6,55 +6,64 @@
     
             <!-------Boxes-and-socials--------------->
             <div class="position-absolute bottom-0 ps-4 d-md-none mb-3" style="z-index: 3">
-                <div class=""><i class="bi bi-instagram text-white social" style="font-size: 1.2rem"></i></div>
-                <div class="mt-2"><i class="bi bi-linkedin text-white social" style="font-size: 1.2rem"></i></div>
-                <div class="mt-2"><i class="bi bi-twitter text-white social" style="font-size: 1.2rem"></i></div>
-                <div class="mt-2 mb-2"><i class="bi bi-facebook text-white social" style="font-size: 1.2rem"></i></div>
+                <div class="scroll-animate"><i class="bi bi-instagram text-white social" style="font-size: 1.2rem"></i></div>
+                <div class="mt-2 scroll-animate"><i class="bi bi-linkedin text-white social" style="font-size: 1.2rem"></i></div>
+                <div class="mt-2 scroll-animate"><i class="bi bi-twitter text-white social" style="font-size: 1.2rem"></i></div>
+                <div class="mt-2 mb-2 scroll-animate"><i class="bi bi-facebook text-white social" style="font-size: 1.2rem"></i></div>
             </div>
        
 
-            <div class="position-absolute bottom-0 ps-4 d-none d-md-block">
-                    <div class=""><i class="bi bi-instagram social" style="font-size: 1.4rem"></i></div>
-                    <div class="mt-3"><i class="bi bi-linkedin social" style="font-size: 1.4rem"></i></div>
-                    <div class="mt-3"><i class="bi bi-twitter social" style="font-size: 1.4rem"></i></div>
-                    <div class="mt-3 mb-3"><i class="bi bi-facebook social" style="font-size: 1.4rem"></i></div>
+            <div class="position-absolute bottom-0 ps-4 d-none d-md-block ">
+                    <div class="scroll-animate"><i class="bi bi-instagram social " style="font-size: 1.4rem"></i></div>
+                    <div class="mt-3 scroll-animate"><i class="bi bi-linkedin social" style="font-size: 1.4rem"></i></div>
+                    <div class="mt-3 scroll-animate"><i class="bi bi-twitter social" style="font-size: 1.4rem"></i></div>
+                    <div class="mt-3 mb-3 scroll-animate"><i class="bi bi-facebook social" style="font-size: 1.4rem"></i></div>
             </div>
             <!-------Boxes--------------->
             <div class="position-absolute bottom-0 pb-3 ps-3">
-                <div class="box-s position-absolute ms-5" style="pointer-events: none;"></div>
-                <div class="box-b" style="pointer-events: none;"></div>
+                <div class="box-s position-absolute ms-5 mb-5" style="pointer-events: none;"></div>
+                <div class="box-b " style="pointer-events: none;"></div>
             </div>
 
             <!-------Tlf-frontpage----------->
             <div class="text-center position-absolute top-30">
-                    <h6 class="font-front-s d-md-none">━━     Velkommen til Nordic Webworks ━━━━━</h6>
+                    <h6 class="font-front-s d-md-none scroll-animate">━━     Velkommen til Nordic Webworks ━━━━━</h6>
                     <h1 class="text-white fw-bold d-md-none font-type">Har <span class="font-drip">din
                         </span> virksomhed brug for en løsning<span class="font-drip">?</span></h1>
 
             <div class="mt-4 d-md-none">
-                <div class="me-3 btn-f fw-bold small d-inline-flex justify-content-center align-items-center text-center m-0 drip">
+                <div class="scroll-animate me-3 btn-f fw-bold small d-inline-flex justify-content-center align-items-center text-center m-0 drip">
                     <span>KONTAKT</div>
-                <div class="btn-f  small d-inline-flex justify-content-center align-items-center text-center m-0">
+                <div class="btn-f scroll-animate small d-inline-flex justify-content-center align-items-center text-center m-0">
                     <span>CASES</span></div>
             </div>
 
 
             <!-------Tablet and pc size----------------------------------->
-            <div class="ms-13">
+            <div class="ms-13 scroll-animate">
                 <!-----TABLET------>
                 <div class="w-51 text-md-start d-none d-md-block d-xl-none">
-                    <h6 class="d-none d-md-block d-flex" style="color: #636363;">━━  Velkommen til Nordic Webworks ━━━━━</h6>
-                    <h1 class="text-white d-none d-md-block front-font-b d-flex font-type">Har <span class="font-drip">din
+                    <h6 class="d-none d-md-block d-flex scroll-animate" style="color: #636363;font-family: 'Source Code Pro'">━━  Velkommen til Nordic Webworks ━━━━━</h6>
+                    <h1 class="text-white d-none d-md-block front-font-b d-flex font-type scroll-animate">Har <span class="font-drip">din
                         </span> virksomhed brug for en løsning<span class="font-drip">?</span></h1>
                 </div>
                 <!-----PC--------->
+                <div class="d-flex justify-content-end m-0 p-0 position-relative">
+                    <div class="position-absolute d-none d-md-block">
+                        <h6 class="text-white-50 fw-light" style="letter-spacing: 0.2rem;writing-mode: vertical-rl;font-family: 'Source Code Pro'">UX-UI</h6>
+                        <h6 class="text-white-50 fw-light mb-5 mt-5" style="letter-spacing: 0.2rem;writing-mode: vertical-rl;font-family: 'Source Code Pro'">Development</h6>
+                        <h6 class="text-white-50 fw-light " style="letter-spacing: 0.2rem;writing-mode: vertical-rl;font-family: 'Source Code Pro'">Design</h6>
+                     <a href="#services"><div class="arrow-box ms-4 mt-5 d-flex align-items-center justify-content-center"><div class="arrow "></div></div></a>
+                    </div>
+                </div>
+
                 <div class="w-43 text-md-start d-md-none d-xl-block">
-                    <h5 class="d-none d-md-block d-flex" style="color: #636363;">━━  Velkommen til Nordic Webworks ━━━━━</h5>
-                    <h1 class="text-white d-none d-md-block front-font-b d-flex font-type">Har <span class="font-drip">din</span>
+                    <h5 class="d-none d-md-block d-flex scroll-animate" style="color: #636363;font-family: 'Source Code Pro'">━━  Velkommen til Nordic Webworks ━━━━━</h5>
+                    <h1 class="text-white d-none d-md-block front-font-b d-flex font-type scroll-animate">Har <span class="font-drip">din</span>
                         virksomhed brug for en løsning<span class="font-drip">?</span></h1>
                 </div>
 
-                <div class="mt-4 text-md-start ms-md-8 d-none d-md-block">
+                <div class="mt-4 text-md-start ms-md-8 d-none d-md-block scroll-animate">
                     <div class="me-3 fw-bold small btn-f d-inline-flex justify-content-center align-items-center text-center m-0 drip">
                         <span>KONTAKT</span>
                     </div>
@@ -62,8 +71,9 @@
                         <span>CASES</span>
                     </div>
                 </div>
-            </div>
 
+
+            </div>
         </div>
 
     </div>
@@ -71,10 +81,10 @@
 
 
 <!-------services---------------------------------->
-<section class="pt-5 mt-2" style="background-color: #252525">
+<section class="pt-5 mt-2 bg-custom-dark" id="services">
     <div class="container">
-        <div class="row d-flex justify-content-center">
-            <div class="col-md-10 col-lg-6 text-center">
+        <div class="row d-flex justify-content-center ">
+            <div class="col-md-10 col-lg-6 text-center scroll-animate">
                 <h1 class="text-white display-6 fw-bold"><span class="text-purple">Services</span> & Ydelser</h1>
                 <p class="text-white-50 small mt-3">
                     Vi tilbyder en bred vifte af ydelser, der er skræddersyet
@@ -86,11 +96,11 @@
     </div>
 </section>
 
-<section id="services" class="services-cards py-md-5" style="background-color: #252525">
+<section id="services" class="services-cards py-md-5 bg-custom-dark">
     <div class="container">
         <div class="row">
             <!-- Card 1 -->
-            <div class="col-md-6 col-lg-3 mb-4">
+            <div class="col-md-6 col-lg-3 mb-4 d-flex justify-content-center scroll-animate">
                 <div class="card border-0 card-hover bg-cases h-100">
                     <div class="card-body text-white d-flex flex-column">
                         <div>
@@ -113,7 +123,7 @@
             </div>
 
             <!-- Card 2 -->
-            <div class="col-md-6 col-lg-3 mb-4 d-flex justify-content-center">
+            <div class="col-md-6 col-lg-3 mb-4 d-flex justify-content-center scroll-animate">
                 <div class="card border-0 card-hover bg-cases h-100">
                     <div class="card-body text-white d-flex flex-column">
                         <div>
@@ -136,7 +146,7 @@
             </div>
 
             <!-- Card 3 -->
-            <div class="col-md-6 col-lg-3 mb-4">
+            <div class="col-md-6 col-lg-3 mb-4 d-flex justify-content-center scroll-animate">
                 <div class="card border-0 card-hover bg-cases h-100">
                     <div class="card-body text-white d-flex flex-column">
                         <div>
@@ -159,7 +169,7 @@
             </div>
 
             <!-- Card 4 -->
-            <div class="col-md-6 col-lg-3 mb-4">
+            <div class="col-md-6 col-lg-3 mb-4 d-flex justify-content-center scroll-animate">
                 <div class="card border-0 card-hover bg-cases h-100">
                     <div class="card-body text-white d-flex flex-column">
                         <div>
@@ -190,13 +200,13 @@
     <!------------Cases-------------->
     <div class="container-fluid m-0 p-0 mb-5">
         <div class="row p-0 m-0 justify-content-center">
-            <div class="row mt-5 ">
-                <h1 class="text-white mt-5 mb-5 font-case col-5 ms-11 d-none d-md-block" style="font-size: 2.7rem"> <span class="fw-light" style="font-size: 1.2rem">Intreasseret i vores erfaringer?</span> <br> Tjek vores tidligere projekter ud!</h1>
-                <h2 class="text-white mt-5 mb-5 font-case col-12 d-md-none" style="font-size: 2rem"> <br> Tjek vores tidligere projekter ud!</h2>
+            <div class="mt-5 ">
+                <h1 class="text-white mt-5 mb-5 font-case col-5 ms-11 d-none d-md-block scroll-animate" style="font-size: 2.7rem"> <span class="fw-light text-white-50" style="font-size: 1.2rem;font-family: 'Source Code Pro'">Intreasseret i vores erfaringer?</span> <br> Tjek vores <span class="text-purple">tidligere projekter</span> ud!</h1>
+                <h2 class="text-white mt-5 mb-4 font-case col-12 d-md-none scroll-animate ps-3" style="font-size: 2rem"> <span class="fw-light text-white-50" style="font-size: 1rem">Intreasseret i vores erfaringer?</span> <br> Tjek vores <span class="text-purple">tidligere projekter</span> ud!</h2>
             </div>
 
 
-            <div class="col-lg-3 col-4 p-0 mt-2 me-2">
+            <div class="col-lg-3 col-4 p-0 mt-2 me-2 scroll-animate">
                 <a class="" href="caseinfo.php"><div class="position-relative img-box">
                     <img src="uploads/chr/case1.webp" alt="#" class="img-box mt-2 p-0" style="height: 15rem">
                     <div class="custom-overlay d-flex">
@@ -208,7 +218,7 @@
                 </div></a>
             </div>
 
-            <div class="col-lg-6 col-6 p-0 mt-2">
+            <div class="col-lg-6 col-6 p-0 mt-2 scroll-animate">
                 <a class="" href="caseinfo.php"><div class="position-relative img-box">
                     <div class="bg-white mt-2 mason-drip d-none" style="height: 17rem"></div>
                     <img src="uploads/chr/case2.webp" alt="#" class="img-box mt-2 p-0" style="height: 15rem">
@@ -221,7 +231,7 @@
                 </div></a>
             </div>
 
-            <div class="col-lg-5 col-7 p-0 me-2">
+            <div class="col-lg-5 col-7 p-0 me-2 scroll-animate">
                 <a class="" href="caseinfo.php"><div class="position-relative img-box">
                         <div class="bg-white mt-2 mason-drip d-none" style="height: 17rem"></div>
                         <img src="uploads/chr/case3.webp" alt="#" class="img-box mt-2 p-0" style="height: 15rem">
@@ -234,7 +244,7 @@
                     </div></a>
             </div>
 
-            <div class="col-lg-4 col-3 p-0">
+            <div class="col-lg-4 col-3 p-0 scroll-animate">
                 <a class="" href="caseinfo.php"><div class="position-relative img-box">
                         <div class="bg-white mt-2 mason-drip d-none" style="height: 17rem"></div>
                         <img src="uploads/chr/case4.webp" alt="#" class="img-box mt-2 p-0" style="height: 15rem">
@@ -247,7 +257,7 @@
                     </div></a>
             </div>
 
-            <div class="col-lg-4 col-4 p-0 me-2">
+            <div class="col-lg-4 col-4 p-0 me-2 scroll-animate">
                 <a class="" href="caseinfo.php"><div class="position-relative img-box">
                         <div class="bg-white mt-2 mason-drip d-none" style="height: 17rem"></div>
                         <img src="uploads/chr/case5.webp" alt="#" class="img-box mt-2 p-0" style="height: 15rem">
@@ -260,7 +270,7 @@
                     </div></a>
             </div>
 
-            <div class="col-lg-5 col-6 p-0">
+            <div class="col-lg-5 col-6 p-0 scroll-animate">
                 <a class="" href="caseinfo.php"><div class="position-relative img-box">
                         <div class="bg-white mt-2 mason-drip d-none" style="height: 17rem"></div>
                         <img src="uploads/chr/case6.webp" alt="#" class="img-box mt-2 p-0" style="height: 15rem">
@@ -279,6 +289,7 @@
 
 
     <script src="js/click..js"></script>
+<script src="js/scroll.js"></script>
 <?php include "includes/footer.php" ?>
 
 
